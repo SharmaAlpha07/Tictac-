29f4e99386e52a0e3656fa12cbe0c2b98a700257
SharmaAlpha07 <166137222+SharmaAlpha07@users.noreply.github.com>
2024-04-09 06:41:32 +0530

    Add files via upload

---
+++ working.js
@@ -0,0 +1,77 @@
+let boxes=document.querySelectorAll(".box");
+let reset=document.querySelector("#reset");
+let newGame=document.querySelector("new-btn");
+let mesgContainer=document.querySelector(".message");
+let msg=document.querySelector("#msg");
+let turnO= true;
+
+const point=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+
+];
+const enable=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+const restart=()=>{
+  turnO=true;
+  enable();
+  mesgContainer.classList.add("hide");
+
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("Box was clicked.")
+       if(turnO){
+        box.innerText="O";
+        turnO=false;
+       }
+       else{
+        box.innerText="X";
+        turnO= true;
+       }
+       box.disabled=true;
+       winner();
+    });
+}) ;
+
+const disable=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+const shoWinner=(winner) =>{
+    mesgContainer.innerText=`Congratulations, winner is ${winner}`;
+    mesgContainer.classList.remove("hide");
+    disable();
+    
+}
+
+const winner=()=>{
+  for(let points of point){
+    
+
+    let pos1=boxes[points[0]].innerText;
+    let pos2=boxes[points[1]].innerText;
+    let pos3=boxes[points[2]].innerText;
+    
+    if(pos1 != ""&& pos2 !=""&& pos3 !=""){
+        if(pos1 === pos2 && pos2 === pos3){
+            console.log("Winner",pos1);
+            shoWinner(pos1);
+        }
+    }
+ }
+};
+newGame.addEventListener("click",restart);
+reset.addEventListener("click",restart);
